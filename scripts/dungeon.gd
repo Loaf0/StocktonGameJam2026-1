c2bcd87855bf516d8db2ce7867c52885d45737e1
Loaf0 <ninjakin102@Outlook.com>
2026-02-07 12:28:54 -0500

    fix

---
+++ scripts/dungeon.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@export var tilemap : TileMapLayer
+
+func _ready() -> void:
+	#give enemies and players tile map reference for movement
+	for node in get_tree().get_nodes_in_group("req_tile_map"):
+		if node.has_method("set_tile_map"):
+			node.set_tile_map(tilemap)
+			print("assigned tile map to " + str(node) )
