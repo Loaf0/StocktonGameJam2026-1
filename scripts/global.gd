d76fe0ad5983f3c9501887f004534cf1c24c98d4
Loaf0 <ninjakin102@Outlook.com>
2026-02-08 14:32:15 -0500

    buffer??? idk i forgot

--- scripts/global.gd
+++ scripts/global.gd
@@ -9,3 +9,13 @@ var two_player_mode : bool = false
 var max_distance_tiles = 16
 
 var occupied_cells : Dictionary = {}
+
+const max_score_multiplier = 6
+var score_multiplier = 1
+var curr_score = 0
+var high_score = 0
+
+func add_score(value : int):
+	curr_score += value * score_multiplier
+
+#connect some signal to show when failed actions
