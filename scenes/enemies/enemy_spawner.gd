d92a637bedb2cf094f38e466c9b673cb9aa65f0e
Loaf0 <ninjakin102@Outlook.com>
2026-02-11 16:09:33 -0500

    merge

---
+++ scenes/enemies/enemy_spawner.gd
@@ -0,0 +1,11 @@
+extends Node
+
+#mushroom candle skeleton
+# difficulty 1.0 + 0.1
+# skeleton - Base Rate 15 - min(base_rate * difficulty, 40)
+# candle - Base Rate 25 - min(base_rate * difficulty, 30)
+# ranged -
+# mushroom = 100 - skeleton - candle
+
+# enemies per room
+# min(1 + ceil(randi(0, difficulty)), 5)
