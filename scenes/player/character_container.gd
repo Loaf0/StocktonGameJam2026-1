5ce68f09b004d5586e76f3581f8233699c9989bd
Loaf0 <ninjakin102@Outlook.com>
2026-02-11 20:50:07 -0500

    hurt + hearts

--- scenes/player/character_container.gd
+++ scenes/player/character_container.gd
@@ -1,10 +1,50 @@
 extends Node2D
 
+@onready var h_box_container: HBoxContainer = $HealthBar/HBoxContainer
+@onready var texture_rect: TextureRect = $HealthBar/HBoxContainer/TextureRect
+const HEART = preload("res://assets/ui/heart.png")
+const NOHEART = preload("res://assets/ui/noheart.png")
+var heart_tweens: Array[Tween] = []
+
 func _ready():
+	BeatManager.beat.connect(_on_beat)
 	%Player1.player_moved.connect(_on_move)
 	%Player2.player_moved.connect(_on_move)
+	Global.health_changed.connect(update_health_bar)
+	update_health_bar()
 
 #clear buffered inputs on move so they dont move at the same time
 func _on_move():
 	%Player1.clear_buffer()
 	%Player2.clear_buffer()
+
+func update_health_bar() -> void:
+	for child in h_box_container.get_children():
+		child.queue_free()
+
+	for i in Global.max_player_health:
+		var heart := TextureRect.new()
+		
+		heart.expand_mode = TextureRect.EXPAND_FIT_WIDTH
+		heart.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT
+		
+		heart.texture = HEART if i < Global.player_health else NOHEART
+		
+		h_box_container.add_child(heart)
+
+func _on_beat(_value: int = 0) -> void:
+	var target_scale = Vector2(0.92, 0.92)
+	var original_scale = Vector2.ONE
+	
+	for heart in h_box_container.get_children():
+		if heart is TextureRect:
+			
+			heart.pivot_offset = heart.size / 2.0
+			
+			var tween := create_tween()
+			tween.tween_property(heart, "scale", target_scale, 0.06)\
+				.set_trans(Tween.TRANS_CUBIC)\
+				.set_ease(Tween.EASE_OUT)
+			tween.tween_property(heart, "scale", original_scale, 0.06)\
+				.set_trans(Tween.TRANS_CUBIC)\
+				.set_ease(Tween.EASE_IN)
--- scripts/global.gd
+++ scripts/global.gd
@@ -1,5 +1,9 @@
 extends Node
 
+signal health_changed
+
+const HURT_SFX = preload("res://assets/audio/kenny/impact/impactPlate_heavy_002.ogg")
+
 var sfx_volume : float = 1.0
 var music_volume : float = 1.0
 
@@ -35,8 +39,21 @@ func _ready() -> void:
 
 func take_damage(damage_amt : int):
 	player_health -= damage_amt
+	health_changed.emit()
+	_play_one_shot_sfx(HURT_SFX, 0.05, 0.0, 0.0,"SFX")
 	if player_health <= 0:
 		death()
 
 func death():
 	pass
+
+func _play_one_shot_sfx(sfx: AudioStream, pitch_range: float = 0.05, start_time: float = 0.0, volume_db: float = 0.0, bus_name: String = "SFX") -> void:
+	var player := AudioStreamPlayer.new()
+	add_child(player)
+	player.stream = sfx
+	player.bus = bus_name
+	pitch_range = clamp(pitch_range, 0.0, 0.08)
+	player.pitch_scale = randf_range(1.0 - pitch_range, 1.0 + pitch_range)
+	player.volume_db = volume_db
+	player.finished.connect(player.queue_free)
+	player.play(start_time)
